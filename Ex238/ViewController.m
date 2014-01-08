e3c3f09048cc9d97096c3782c016a40a08ef0dea
saerem21 <nemessisdds>
2014-01-08 14:40:14 +0900

    터치이벤트 처리 예제입니다,...
    238page

--- Ex238/ViewController.m
+++ Ex238/ViewController.m
@@ -9,10 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    BOOL holdingImage;
+}
+@property (weak,nonatomic)IBOutlet UIImageView *ballImage;
 @end
 
 @implementation ViewController
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+    if(CGRectContainsPoint(self.ballImage.frame, point)){
+        holdingImage =YES;
+        self.ballImage.transform = CGAffineTransformMakeScale(1.2, 1.2);
+    }
+ }
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"moved count %d",[[event allTouches] count]);
+    
+    if(holdingImage){
+        UITouch *touch = [touches anyObject];
+        CGPoint point = [touch locationInView:self.view];
+        self.ballImage.center = point;
+    }
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    if(holdingImage){
+        self.ballImage.transform = CGAffineTransformIdentity;
+        holdingImage = NO;
+    }
+}
 
 - (void)viewDidLoad
 {
